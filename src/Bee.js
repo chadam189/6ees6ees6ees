262a604e199e6add898681827f5dfa3455926571
Adam Miller <chadam189@gmail.com>
2017-10-25 10:46:10 -0700

    Pass all tests for all classes using ES6 syntax

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,17 @@
-class Bee {
+class Bee extends Grub {
   // TODO..
+  constructor () {
+  	super ();
+  	this.age = 5;
+  	this.color = 'yellow';
+  	this.job = 'Keep on growing';
+  }
 };
+
+
+// class Tesla extends Car {
+//   move () {
+//     super.move()
+//     this.distance += 4
+//   }
+// }
